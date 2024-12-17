4973af31a44aa0bd09bc65d01b1134fd73b17020
Mohammed yaser <100796943+mohammedyaserr@users.noreply.github.com>
2024-12-17 17:21:23 +0530

    Delete hostel mangment system directory

--- hostel mangment system/roomginie/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from . models import studentregister,parentregister
-admin.site.register(studentregister)
-admin.site.register(parentregister)
-# Register your models here.
--- hostel mangment system/roomginie/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RoomgenieConfig(AppConfig):
-    name = 'RoomGenie'
